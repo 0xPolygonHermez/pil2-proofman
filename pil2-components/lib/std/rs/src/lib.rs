ca229d2c07c0c986e2c16cd9ec7a15f45f68ca8e
Héctor Masip <hmasip@polygon.technology>
2024-11-28 11:54:28 +0000

    cleaning

--- pil2-components/lib/std/rs/src/lib.rs
+++ pil2-components/lib/std/rs/src/lib.rs
@@ -1,7 +1,5 @@
 mod debug;
 mod decider;
-// mod div_lib;
-// mod std_ops;
 mod range_check;
 mod std;
 mod std_prod;
@@ -9,8 +7,6 @@ mod std_sum;
 
 pub use debug::*;
 pub use decider::*;
-// pub use div_lib::*;
-// pub use std_ops::*;
 pub use range_check::*;
 pub use std::*;
 pub use std_prod::*;
