2ec2ff1d59bb6bea1857546d73294de195a6b6cd
Héctor Masip Ardevol <hmasip@polygon.technology>
2025-01-17 13:56:38 +0100

    Allowing for different len in fibosq (#145)
    * Allowing for different len in fibosq
    
    * Module to N = 14
    
    ---------
    
    Co-authored-by: RogerTaule <roger.taulee@gmail.com>

--- examples/fibonacci-square/src/module.rs
+++ examples/fibonacci-square/src/module.rs
@@ -6,7 +6,7 @@ use pil_std_lib::Std;
 use p3_field::{AbstractField, PrimeField64};
 use num_bigint::BigInt;
 
-use crate::{BuildPublicValues, ModuleTrace};
+use crate::{BuildPublicValues, ModuleTrace, ModuleAirValues};
 
 pub struct Module<F: PrimeField64> {
     inputs: Mutex<Vec<(u64, u64)>>,
@@ -38,39 +38,46 @@ impl<F: PrimeField64 + AbstractField + Copy> WitnessComponent<F> for Module<F> {
         let publics = BuildPublicValues::from_vec_guard(pctx.get_publics());
         let module = F::as_canonical_u64(&publics.module);
 
-        let mut trace = ModuleTrace::new_zeroes();
-
         //range_check(colu: mod - x_mod, min: 1, max: 2**8-1);
         let range = self.std_lib.get_range(BigInt::from(1), BigInt::from((1 << 8) - 1), None);
 
         let inputs = self.inputs.lock().unwrap();
 
-        let inputs_len = inputs.len();
+        let num_rows = ModuleTrace::<F>::NUM_ROWS;
+        let num_instances = inputs.len().div_ceil(num_rows);
 
-        let mut x_mods = Vec::new();
+        for j in 0..num_instances {
+            let mut trace = ModuleTrace::new_zeroes();
 
-        for (i, input) in inputs.iter().enumerate() {
-            let x = input.0;
-            let q = x / module;
-            let x_mod = input.1;
+            let inputs_slice = if j < num_instances - 1 {
+                inputs[j * num_rows..(j + 1) * num_rows].to_vec()
+            } else {
+                inputs[j * num_rows..].to_vec()
+            };
 
-            trace[i].x = F::from_canonical_u64(x);
-            trace[i].q = F::from_canonical_u64(q);
-            trace[i].x_mod = F::from_canonical_u64(x_mod);
-            x_mods.push(x_mod);
-        }
+            for (i, input) in inputs_slice.iter().enumerate() {
+                let x = input.0;
+                let q = x / module;
+                let x_mod = input.1;
 
-        let air_instance = AirInstance::new_from_trace(FromTrace::new(&mut trace));
-        let is_mine = add_air_instance::<F>(air_instance, pctx.clone());
-        if is_mine {
-            for x_mod in x_mods.iter() {
+                trace[i].x = F::from_canonical_u64(x);
+                trace[i].q = F::from_canonical_u64(q);
+                trace[i].x_mod = F::from_canonical_u64(x_mod);
+
+                // Check if x_mod is in the range
                 self.std_lib.range_check(F::from_canonical_u64(module - x_mod), F::one(), range);
             }
 
             // Trivial range check for the remaining rows
-            for _ in inputs_len..trace.num_rows() {
+            for _ in inputs_slice.len()..trace.num_rows() {
                 self.std_lib.range_check(F::from_canonical_u64(module), F::one(), range);
             }
+
+            let mut air_values = ModuleAirValues::<F>::new();
+            air_values.last_segment = F::from_bool(j == num_instances - 1);
+
+            let air_instance = AirInstance::new_from_trace(FromTrace::new(&mut trace).with_air_values(&mut air_values));
+            add_air_instance::<F>(air_instance, pctx.clone());
         }
     }
 }
