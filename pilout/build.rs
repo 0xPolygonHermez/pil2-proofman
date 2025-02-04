b1ea2ae2c4892b6258c3cc0c6c11f178bc763103
Sam Johnson <sam@durosoft.com>
2025-02-04 01:13:08 -0500

    have pilout automatically derive serde traits

--- pilout/build.rs
+++ pilout/build.rs
@@ -2,6 +2,13 @@ extern crate prost_build;
 
 fn main() {
     let mut config = prost_build::Config::new();
+
+    // Enable serde Serialize/Deserialize for all types
+    config.type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]");
+
+    // Enable experimental proto3 optional fields
     config.protoc_arg("--experimental_allow_proto3_optional");
+
+    // Compile Protobuf definitions
     config.compile_protos(&["src/pilout.proto"], &["src/"]).unwrap();
 }
