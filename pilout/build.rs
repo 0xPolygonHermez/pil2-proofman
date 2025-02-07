512fb947c911274199fa072d6c03d015e0d48281
Sam Johnson <sam@durosoft.com>
2025-02-07 03:21:47 -0500

    use camel case

--- pilout/build.rs
+++ pilout/build.rs
@@ -3,8 +3,8 @@ extern crate prost_build;
 fn main() {
     let mut config = prost_build::Config::new();
 
-    // Enable serde Serialize/Deserialize for all types
-    config.type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]");
+    // Enable serde Serialize/Deserialize for all types and enforce camelCase
+    config.type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)] #[serde(rename_all = \"camelCase\")]");
 
     // Enable experimental proto3 optional fields
     config.protoc_arg("--experimental_allow_proto3_optional");
