d7781fc3b3078c6a818a9b8c1626c6c832cd2243
Héctor Masip <hmasip@polygon.technology>
2025-03-06 12:12:26 +0000

    Some optis to extension

--- curves/src/goldilocks_quintic_extension.rs
+++ curves/src/goldilocks_quintic_extension.rs
@@ -1,4 +1,3 @@
-use num_bigint::BigUint;
 
 use p3_goldilocks::Goldilocks;
 use p3_field::{BasedVectorSpace, ExtensionField, Field, PrimeCharacteristicRing};
@@ -11,7 +10,6 @@ pub type GoldilocksQuinticExtension = BinomialExtensionField<Goldilocks, 5>;
 // as described in https://hackmd.io/CxJrIhv-SP65W3GWS_J5bw?view#Extension-Field-Selection
 // which is inspired by https://github.com/succinctlabs/sp1/blob/dev/crates/stark/src/septic_extension.rs
 pub trait Squaring {
-    const EXP: u64 = 0x7FFF_FFFF_8000_0000; // (p-1)/2
 
     fn gammas1(i: usize) -> Goldilocks;
 
@@ -22,6 +20,8 @@ pub trait Squaring {
     fn second_frobenius(&self) -> Self;
 
     fn exp_fifth_cyclotomic(&self) -> Self;
+
+    fn is_square_base(x: &Goldilocks) -> bool;
 
     fn is_square(&self) -> (Goldilocks, bool);
 
@@ -85,17 +85,23 @@ impl Squaring for GoldilocksQuinticExtension {
         *self * t0 * t1 // self^(p⁴ + p³ + p² + p + 1)
     }
 
+    // Computes the Legendre symbol (x / p) = (p-1)/2 and checks if its 1, assumes x != 0
+    fn is_square_base(x: &Goldilocks) -> bool {
+        // (p-1)/2 = 2^63 - 2^31 -> x^((p-1)/2) = x^(2^63) / x^(2^31)
+        let exp_63 = x.exp_power_of_2(63);
+        let exp_31 = x.exp_power_of_2(31);
+        let symbol = exp_63 / exp_31;
+        symbol == Goldilocks::ONE
+    }
+
     // Computes self^((p⁵ - 1)/2), assumes self != 0
     fn is_square(&self) -> (Goldilocks, bool) {
         // Compute a = self^(p⁴ + p³ + p² + p + 1), a ∈ Fp
         let pow_fifth_cyclo: Goldilocks =
             self.exp_fifth_cyclotomic().as_base().expect("This should be a base field element");
 
-        // Compute a^((p - 1)/2)
-        let pow = pow_fifth_cyclo.exp_u64(Self::EXP);
-
-        // Check if self^((p⁵ - 1)/2) == 1
-        (pow_fifth_cyclo, pow == Goldilocks::ONE)
+        // Checks whether a is a square in Fp
+        (pow_fifth_cyclo, Self::is_square_base(&pow_fifth_cyclo))
     }
 
     // We compute the square root using the identity:
@@ -127,20 +133,21 @@ impl Squaring for GoldilocksQuinticExtension {
         let g = Goldilocks::GENERATOR;
         let mut a = Goldilocks::ONE;
         let mut nonresidue = a - n;
-        while nonresidue.exp_u64(Self::EXP) == Goldilocks::ONE {
+        while Self::is_square_base(&nonresidue) {
             a *= g;
             nonresidue = a.square() - n;
         }
 
         // 2] Compute (a + sqrt(a² - n))^((p+1)/2)
-        let exp = BigUint::from(Self::EXP + 1);
         let mut x = CipollaExtension::new(a, Goldilocks::ONE);
-        x = x.pow(&exp, nonresidue);
+        x = x.exp(nonresidue);
 
         // Second Part: Compute self^(((p+1)/2)p³ + ((p+1)/2)p + 1)
         // ================================================================
-        // 1] Compute self^((p+1)/2)
-        let pow = self.exp_u64(Self::EXP + 1);
+        // 1] Compute self^((p+1)/2). Notice (p+1)/2 = 2^63 - 2^31 + 1
+        let pow_63 = self.exp_power_of_2(63);
+        let pow_31 = self.exp_power_of_2(31);
+        let pow = *self * pow_63 / pow_31;
 
         // 2] Compute the rest using Frobenius
         let mut pow_frob = pow.first_frobenius(); // self^(((p+1)/2)p)
@@ -153,8 +160,8 @@ impl Squaring for GoldilocksQuinticExtension {
     }
 }
 
-// Extension field for Cipolla's algorithm, taken from https://github.com/Plonky3/Plonky3/pull/439/files
-// adapted to the new Plonky3 API
+// Extension field for Cipolla's algorithm, adapted from https://github.com/Plonky3/Plonky3/pull/439/files
+// Cipolla extension is defined as Fp[sqrt(a² - n)], where a² - n is a non-residue in Fp
 #[derive(Clone, Copy, Debug)]
 struct CipollaExtension<F: Field> {
     real: F,
@@ -166,10 +173,6 @@ impl<F: Field> CipollaExtension<F> {
         Self { real, imag }
     }
 
-    fn one() -> Self {
-        Self::new(F::ONE, F::ZERO)
-    }
-
     fn mul(&self, other: Self, nonresidue: F) -> Self {
         Self::new(
             self.real * other.real + nonresidue * self.imag * other.imag,
@@ -177,18 +180,33 @@ impl<F: Field> CipollaExtension<F> {
         )
     }
 
-    fn pow(&self, exp: &BigUint, nonresidue: F) -> Self {
-        let mut result = Self::one();
-        let mut base = *self;
-        let bits = exp.bits();
+    fn square(&self, nonresidue: F) -> Self {
+        let real = self.real.square() + nonresidue * self.imag.square();
+        let imag = F::TWO * self.real * self.imag;
+        Self::new(real, imag)
+    }
 
-        for i in 0..bits {
-            if exp.bit(i) {
-                result = result.mul(base, nonresidue);
-            }
-            base = base.mul(base, nonresidue);
+    fn div(&self, other: Self, nonresidue: F) -> Self {
+        let denom = other.real.square() - nonresidue * other.imag.square();
+        let real = (self.real * other.real - nonresidue * self.imag * other.imag) / denom;
+        let imag = (self.imag * other.real - self.real * other.imag) / denom;
+        Self::new(real, imag)
+    }
+
+    fn exp_power_of_2(&self, power_log: usize, nonresidue: F) -> Self {
+        let mut res = *self;
+        for _ in 0..power_log {
+            res = res.square(nonresidue);
         }
-        result
+        res
+    }
+
+    // Computes exponentiation by (p+1)/2 = 2^63 - 2^31 + 1
+    fn exp(&self, nonresidue: F) -> Self {
+        let pow_63 = self.exp_power_of_2(63, nonresidue);
+        let pow_31 = self.exp_power_of_2(31, nonresidue);
+        let pow = pow_63.div(pow_31, nonresidue);
+        self.mul(pow, nonresidue)
     }
 }
 
