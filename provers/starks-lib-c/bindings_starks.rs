2a6547b4256b910eefe0714fbc6fd3d749783726
rickb80 <75077385+rickb80@users.noreply.github.com>
2025-04-23 11:01:36 +0000

    final cleans

--- provers/starks-lib-c/bindings_starks.rs
+++ provers/starks-lib-c/bindings_starks.rs
@@ -1,4 +1,4 @@
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z15save_challengesPvPcS0_"]
     pub fn save_challenges(
         pChallenges: *mut ::std::os::raw::c_void,
@@ -6,7 +6,7 @@ unsafe extern "C" {
         fileDir: *mut ::std::os::raw::c_char,
     );
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z12save_publicsmPvPc"]
     pub fn save_publics(
         numPublicInputs: ::std::os::raw::c_ulong,
@@ -14,7 +14,7 @@ unsafe extern "C" {
         fileDir: *mut ::std::os::raw::c_char,
     );
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z17save_proof_valuesPvPcS0_"]
     pub fn save_proof_values(
         pProofValues: *mut ::std::os::raw::c_void,
@@ -22,12 +22,12 @@ unsafe extern "C" {
         fileDir: *mut ::std::os::raw::c_char,
     );
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z15n_hints_by_namePvPc"]
     pub fn n_hints_by_name(p_expression_bin: *mut ::std::os::raw::c_void, hintName: *mut ::std::os::raw::c_char)
         -> u64;
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z20get_hint_ids_by_namePvPmPc"]
     pub fn get_hint_ids_by_name(
         p_expression_bin: *mut ::std::os::raw::c_void,
@@ -35,7 +35,7 @@ unsafe extern "C" {
         hintName: *mut ::std::os::raw::c_char,
     );
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z14stark_info_newPcbbbb"]
     pub fn stark_info_new(
         filename: *mut ::std::os::raw::c_char,
@@ -45,31 +45,31 @@ unsafe extern "C" {
         gpu: bool,
     ) -> *mut ::std::os::raw::c_void;
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z14get_proof_sizePv"]
     pub fn get_proof_size(pStarkInfo: *mut ::std::os::raw::c_void) -> u64;
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z22set_memory_expressionsPvmm"]
     pub fn set_memory_expressions(pStarkInfo: *mut ::std::os::raw::c_void, nTmp1: u64, nTmp3: u64);
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z15get_map_total_nPv"]
     pub fn get_map_total_n(pStarkInfo: *mut ::std::os::raw::c_void) -> u64;
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z21get_const_pols_offsetPv"]
     pub fn get_const_pols_offset(pStarkInfo: *mut ::std::os::raw::c_void) -> u64;
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z36get_map_total_n_custom_commits_fixedPv"]
     pub fn get_map_total_n_custom_commits_fixed(pStarkInfo: *mut ::std::os::raw::c_void) -> u64;
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z15stark_info_freePv"]
     pub fn stark_info_free(pStarkInfo: *mut ::std::os::raw::c_void);
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z15load_const_treePvS_PcmS0_"]
     pub fn load_const_tree(
         pStarkInfo: *mut ::std::os::raw::c_void,
@@ -79,7 +79,7 @@ unsafe extern "C" {
         verkeyFilename: *mut ::std::os::raw::c_char,
     ) -> bool;
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z15load_const_polsPvPcm"]
     pub fn load_const_pols(
         pConstPols: *mut ::std::os::raw::c_void,
@@ -87,15 +87,15 @@ unsafe extern "C" {
         constSize: u64,
     );
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z19get_const_tree_sizePv"]
     pub fn get_const_tree_size(pStarkInfo: *mut ::std::os::raw::c_void) -> u64;
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z14get_const_sizePv"]
     pub fn get_const_size(pStarkInfo: *mut ::std::os::raw::c_void) -> u64;
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z20calculate_const_treePvS_S_"]
     pub fn calculate_const_tree(
         pStarkInfo: *mut ::std::os::raw::c_void,
@@ -103,7 +103,7 @@ unsafe extern "C" {
         pConstTree: *mut ::std::os::raw::c_void,
     );
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z16write_const_treePvS_Pc"]
     pub fn write_const_tree(
         pStarkInfo: *mut ::std::os::raw::c_void,
@@ -111,7 +111,7 @@ unsafe extern "C" {
         treeFilename: *mut ::std::os::raw::c_char,
     );
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z19expressions_bin_newPcbb"]
     pub fn expressions_bin_new(
         filename: *mut ::std::os::raw::c_char,
@@ -119,27 +119,27 @@ unsafe extern "C" {
         verifier: bool,
     ) -> *mut ::std::os::raw::c_void;
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z14get_max_n_tmp1Pv"]
     pub fn get_max_n_tmp1(pExpressionsBin: *mut ::std::os::raw::c_void) -> u64;
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z14get_max_n_tmp3Pv"]
     pub fn get_max_n_tmp3(pExpressionsBin: *mut ::std::os::raw::c_void) -> u64;
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z12get_max_argsPv"]
     pub fn get_max_args(pExpressionsBin: *mut ::std::os::raw::c_void) -> u64;
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z11get_max_opsPv"]
     pub fn get_max_ops(pExpressionsBin: *mut ::std::os::raw::c_void) -> u64;
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z20expressions_bin_freePv"]
     pub fn expressions_bin_free(pExpressionsBin: *mut ::std::os::raw::c_void);
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z14get_hint_fieldPvS_S_mPcS_"]
     pub fn get_hint_field(
         pSetupCtx: *mut ::std::os::raw::c_void,
@@ -150,7 +150,7 @@ unsafe extern "C" {
         hintOptions: *mut ::std::os::raw::c_void,
     );
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z21get_hint_field_valuesPvmPc"]
     pub fn get_hint_field_values(
         pSetupCtx: *mut ::std::os::raw::c_void,
@@ -158,7 +158,7 @@ unsafe extern "C" {
         hintFieldName: *mut ::std::os::raw::c_char,
     ) -> u64;
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z20get_hint_field_sizesPvS_mPcS_"]
     pub fn get_hint_field_sizes(
         pSetupCtx: *mut ::std::os::raw::c_void,
@@ -168,7 +168,7 @@ unsafe extern "C" {
         hintOptions: *mut ::std::os::raw::c_void,
     );
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z15mul_hint_fieldsPvS_mPmPPcS2_S2_PS_S3_"]
     pub fn mul_hint_fields(
         pSetupCtx: *mut ::std::os::raw::c_void,
@@ -182,7 +182,7 @@ unsafe extern "C" {
         hintOptions2: *mut *mut ::std::os::raw::c_void,
     );
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z14acc_hint_fieldPvS_mPcS0_S0_b"]
     pub fn acc_hint_field(
         pSetupCtx: *mut ::std::os::raw::c_void,
@@ -194,7 +194,7 @@ unsafe extern "C" {
         add: bool,
     );
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z19acc_mul_hint_fieldsPvS_mPcS0_S0_S0_S_S_b"]
     pub fn acc_mul_hint_fields(
         pSetupCtx: *mut ::std::os::raw::c_void,
@@ -209,7 +209,7 @@ unsafe extern "C" {
         add: bool,
     );
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z20update_airgroupvaluePvS_mPcS0_S0_S_S_b"]
     pub fn update_airgroupvalue(
         pSetupCtx: *mut ::std::os::raw::c_void,
@@ -223,7 +223,7 @@ unsafe extern "C" {
         add: bool,
     ) -> u64;
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z14set_hint_fieldPvS_S_mPc"]
     pub fn set_hint_field(
         pSetupCtx: *mut ::std::os::raw::c_void,
@@ -233,7 +233,7 @@ unsafe extern "C" {
         hintFieldName: *mut ::std::os::raw::c_char,
     ) -> u64;
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z11get_hint_idPvmPc"]
     pub fn get_hint_id(
         pSetupCtx: *mut ::std::os::raw::c_void,
@@ -241,7 +241,7 @@ unsafe extern "C" {
         hintFieldName: *mut ::std::os::raw::c_char,
     ) -> u64;
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z28calculate_impols_expressionsPvmS_"]
     pub fn calculate_impols_expressions(
         pSetupCtx: *mut ::std::os::raw::c_void,
@@ -249,7 +249,7 @@ unsafe extern "C" {
         stepsParams: *mut ::std::os::raw::c_void,
     );
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z18load_custom_commitPvmS_Pc"]
     pub fn load_custom_commit(
         pSetup: *mut ::std::os::raw::c_void,
@@ -258,7 +258,7 @@ unsafe extern "C" {
         customCommitFile: *mut ::std::os::raw::c_char,
     );
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z19write_custom_commitPvmmmS_Pcb"]
     pub fn write_custom_commit(
         root: *mut ::std::os::raw::c_void,
@@ -270,7 +270,7 @@ unsafe extern "C" {
         check: bool,
     );
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z14commit_witnessmmmmPvS_S_S_j"]
     pub fn commit_witness(
         arity: u64,
@@ -284,7 +284,7 @@ unsafe extern "C" {
         mpi_node_rank: u32,
     );
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z14calculate_hashPvS_mm"]
     pub fn calculate_hash(
         pValue: *mut ::std::os::raw::c_void,
@@ -293,35 +293,35 @@ unsafe extern "C" {
         nOutputs: u64,
     );
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z14transcript_newmb"]
     pub fn transcript_new(arity: u64, custom: bool) -> *mut ::std::os::raw::c_void;
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z14transcript_addPvS_m"]
     pub fn transcript_add(pTranscript: *mut ::std::os::raw::c_void, pInput: *mut ::std::os::raw::c_void, size: u64);
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z15transcript_freePv"]
     pub fn transcript_free(pTranscript: *mut ::std::os::raw::c_void);
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z13get_challengePvS_"]
     pub fn get_challenge(pTranscript: *mut ::std::os::raw::c_void, pElement: *mut ::std::os::raw::c_void);
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z17get_n_constraintsPv"]
     pub fn get_n_constraints(pSetupCtx: *mut ::std::os::raw::c_void) -> u64;
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z27get_constraints_lines_sizesPvPm"]
     pub fn get_constraints_lines_sizes(pSetupCtx: *mut ::std::os::raw::c_void, constraintsLinesSizes: *mut u64);
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z21get_constraints_linesPvPPh"]
     pub fn get_constraints_lines(pSetupCtx: *mut ::std::os::raw::c_void, constraintsLines: *mut *mut u8);
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z18verify_constraintsPvS_S_"]
     pub fn verify_constraints(
         pSetupCtx: *mut ::std::os::raw::c_void,
@@ -329,22 +329,22 @@ unsafe extern "C" {
         constraintsInfo: *mut ::std::os::raw::c_void,
     );
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z24get_n_global_constraintsPv"]
     pub fn get_n_global_constraints(p_globalinfo_bin: *mut ::std::os::raw::c_void) -> u64;
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z34get_global_constraints_lines_sizesPvPm"]
     pub fn get_global_constraints_lines_sizes(
         p_globalinfo_bin: *mut ::std::os::raw::c_void,
         constraintsLinesSizes: *mut u64,
     );
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z28get_global_constraints_linesPvPPh"]
     pub fn get_global_constraints_lines(p_globalinfo_bin: *mut ::std::os::raw::c_void, constraintsLines: *mut *mut u8);
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z25verify_global_constraintsPcPvS0_S0_S0_PS0_S0_"]
     pub fn verify_global_constraints(
         globalInfoFile: *mut ::std::os::raw::c_char,
@@ -356,7 +356,7 @@ unsafe extern "C" {
         globalConstraintsInfo: *mut ::std::os::raw::c_void,
     );
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z40get_hint_field_global_constraints_valuesPvmPc"]
     pub fn get_hint_field_global_constraints_values(
         p_globalinfo_bin: *mut ::std::os::raw::c_void,
@@ -364,7 +364,7 @@ unsafe extern "C" {
         hintFieldName: *mut ::std::os::raw::c_char,
     ) -> u64;
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z39get_hint_field_global_constraints_sizesPcPvS0_mS_b"]
     pub fn get_hint_field_global_constraints_sizes(
         globalInfoFile: *mut ::std::os::raw::c_char,
@@ -375,7 +375,7 @@ unsafe extern "C" {
         print_expression: bool,
     );
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z33get_hint_field_global_constraintsPcPvS0_S0_S0_S0_PS0_mS_b"]
     pub fn get_hint_field_global_constraints(
         globalInfoFile: *mut ::std::os::raw::c_char,
@@ -390,7 +390,7 @@ unsafe extern "C" {
         print_expression: bool,
     );
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z33set_hint_field_global_constraintsPcPvS0_S0_mS_"]
     pub fn set_hint_field_global_constraints(
         globalInfoFile: *mut ::std::os::raw::c_char,
@@ -401,7 +401,7 @@ unsafe extern "C" {
         hintFieldName: *mut ::std::os::raw::c_char,
     ) -> u64;
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z9gen_proofPvmmmS_S_PmPcS_bj"]
     pub fn gen_proof(
         pSetupCtx: *mut ::std::os::raw::c_void,
@@ -417,7 +417,7 @@ unsafe extern "C" {
         mpi_node_rank: u32,
     );
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z19gen_recursive_proofPvPcmmmS_S_S_S_S_PmS0_bS_bj"]
     pub fn gen_recursive_proof(
         pSetupCtx: *mut ::std::os::raw::c_void,
@@ -438,7 +438,7 @@ unsafe extern "C" {
         mpi_node_rank: u32,
     );
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z18get_committed_polsPvPcS_S_mmmm"]
     pub fn get_committed_pols(
         circomWitness: *mut ::std::os::raw::c_void,
@@ -451,7 +451,7 @@ unsafe extern "C" {
         nCols: u64,
     );
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z25gen_recursive_proof_finalPvPcmmmS_S_S_S_S_S0_"]
     pub fn gen_recursive_proof_final(
         pSetupCtx: *mut ::std::os::raw::c_void,
@@ -467,7 +467,7 @@ unsafe extern "C" {
         proof_file: *mut ::std::os::raw::c_char,
     ) -> *mut ::std::os::raw::c_void;
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z21gen_final_snark_proofPvPcS0_"]
     pub fn gen_final_snark_proof(
         circomWitnessFinal: *mut ::std::os::raw::c_void,
@@ -475,11 +475,11 @@ unsafe extern "C" {
         outputDir: *mut ::std::os::raw::c_char,
     );
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z11setLogLevelm"]
     pub fn setLogLevel(level: u64);
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z12stark_verifyPmPvS0_PcS0_S0_S0_"]
     pub fn stark_verify(
         jProof: *mut u64,
@@ -491,7 +491,7 @@ unsafe extern "C" {
         challenges: *mut ::std::os::raw::c_void,
     ) -> bool;
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z18stark_verify_bn128PvS_S_PcS_"]
     pub fn stark_verify_bn128(
         jProof: *mut ::std::os::raw::c_void,
@@ -501,7 +501,7 @@ unsafe extern "C" {
         pPublics: *mut ::std::os::raw::c_void,
     ) -> bool;
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z22stark_verify_from_filePcPvS0_S_S0_S0_S0_"]
     pub fn stark_verify_from_file(
         proof: *mut ::std::os::raw::c_char,
@@ -513,7 +513,7 @@ unsafe extern "C" {
         challenges: *mut ::std::os::raw::c_void,
     ) -> bool;
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z20write_fixed_cols_binPcS_S_mmPv"]
     pub fn write_fixed_cols_bin(
         binFile: *mut ::std::os::raw::c_char,
@@ -524,26 +524,26 @@ unsafe extern "C" {
         fixedPolsInfo: *mut ::std::os::raw::c_void,
     );
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z19get_omp_max_threadsv"]
     pub fn get_omp_max_threads() -> u64;
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z19set_omp_num_threadsm"]
     pub fn set_omp_num_threads(num_threads: u64);
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z25gen_device_commit_buffersPvj"]
     pub fn gen_device_commit_buffers(
         maxSizes: *mut ::std::os::raw::c_void,
         mpi_node_rank: u32,
     ) -> *mut ::std::os::raw::c_void;
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z30gen_device_commit_buffers_freePvj"]
     pub fn gen_device_commit_buffers_free(d_buffers: *mut ::std::os::raw::c_void, mpi_node_rank: u32);
 }
-unsafe extern "C" {
+extern "C" {
     #[link_name = "\u{1}_Z10set_devicej"]
     pub fn set_device(mpi_node_rank: u32);
 }
