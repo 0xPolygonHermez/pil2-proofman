960a0ae8ee48b97c45ded6d66adeeff3896d048d
Xavier Pinsach <10213118+xavi-pinsach@users.noreply.github.com>
2025-05-15 13:19:58 +0000

    added goldilocks calls to own functions, removed p3_goldilocks

--- fields/src/goldilocks.rs
+++ fields/src/goldilocks.rs
@@ -5,37 +5,31 @@ use core::ops::{Add, AddAssign, Div, Mul, MulAssign, Neg, Sub, SubAssign};
 use std::ops::DivAssign;
 
 use num_bigint::BigUint;
-use p3_goldilocks::Goldilocks as P3Goldilocks;
-use p3_field::Field as P3Field;
-use p3_field::PrimeField64 as P3PrimeField64;
-use p3_field::PrimeCharacteristicRing as P3PrimeCharacteristicRing;
 
+use proofman_starks_lib_c::{
+    goldilocks_add_assign_ffi, goldilocks_add_ffi, goldilocks_div_assign_ffi, goldilocks_div_ffi, goldilocks_inv_ffi,
+    goldilocks_mul_assign_ffi, goldilocks_mul_ffi, goldilocks_neg_ffi, goldilocks_sub_assign_ffi, goldilocks_sub_ffi,
+};
 use rand::distr::{Distribution, StandardUniform};
 use rand::Rng;
 use serde::{Deserialize, Serialize};
 
 use crate::{quotient_map_small_int, Field, PrimeField, PrimeField64, QuotientMap};
 
-const P: u64 = 0xFFFF_FFFF_0000_0001;
-
 #[derive(Copy, Clone, Default, Serialize, Deserialize)]
-pub struct Goldilocks {
-    pub(crate) value: P3Goldilocks,
-}
+pub struct Goldilocks(u64);
 
 impl Goldilocks {
-    pub(crate) const fn new(value: P3Goldilocks) -> Self {
-        Self { value }
-    }
+    const P: u64 = 0xFFFF_FFFF_0000_0001;
 
-    pub(crate) fn new_from_u64(value: u64) -> Self {
-        Self::new(P3Goldilocks::from_u64(value))
+    pub(crate) const fn new(value: u64) -> Self {
+        Self(value)
     }
 }
 
 impl PartialEq for Goldilocks {
     fn eq(&self, other: &Self) -> bool {
-        self.value.as_canonical_u64() == other.value.as_canonical_u64()
+        self.as_canonical_u64() == other.as_canonical_u64()
     }
 }
 
@@ -61,13 +55,13 @@ impl PartialOrd for Goldilocks {
 
 impl Display for Goldilocks {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        Display::fmt(&self.value, f)
+        Display::fmt(&self.0, f)
     }
 }
 
 impl Debug for Goldilocks {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        Debug::fmt(&self.value, f)
+        Debug::fmt(&self.0, f)
     }
 }
 
@@ -77,17 +71,17 @@ quotient_map_small_int!(Goldilocks, i64, [i8, i16, i32]);
 impl QuotientMap<u64> for Goldilocks {
     #[inline]
     fn from_int(int: u64) -> Self {
-        Self::new_from_u64(int)
+        Self::new(int)
     }
 
     #[inline]
     fn from_canonical_checked(int: u64) -> Option<Self> {
-        (int < Self::ORDER_U64).then(|| Self::new_from_u64(int))
+        (int < Self::ORDER_U64).then(|| Self::new(int))
     }
 
     #[inline(always)]
     unsafe fn from_canonical_unchecked(int: u64) -> Self {
-        Self::new_from_u64(int)
+        Self::new(int)
     }
 }
 
@@ -95,19 +89,19 @@ impl QuotientMap<i64> for Goldilocks {
     #[inline]
     fn from_int(int: i64) -> Self {
         if int >= 0 {
-            Self::new_from_u64(int as u64)
+            Self::new(int as u64)
         } else {
-            Self::new_from_u64(Self::ORDER_U64.wrapping_add_signed(int))
+            Self::new(Self::ORDER_U64.wrapping_add_signed(int))
         }
     }
 
     #[inline]
     fn from_canonical_checked(int: i64) -> Option<Self> {
-        const POS_BOUND: i64 = (P >> 1) as i64;
+        const POS_BOUND: i64 = (Goldilocks::P >> 1) as i64;
         const NEG_BOUND: i64 = -POS_BOUND;
         match int {
-            0..=POS_BOUND => Some(Self::new_from_u64(int as u64)),
-            NEG_BOUND..0 => Some(Self::new_from_u64(Self::ORDER_U64.wrapping_add_signed(int))),
+            0..=POS_BOUND => Some(Self::new(int as u64)),
+            NEG_BOUND..0 => Some(Self::new(Self::ORDER_U64.wrapping_add_signed(int))),
             _ => None,
         }
     }
@@ -119,49 +113,61 @@ impl QuotientMap<i64> for Goldilocks {
 }
 
 impl Field for Goldilocks {
-    const ZERO: Self = Self::new(P3Goldilocks::ZERO);
-    const ONE: Self = Self::new(P3Goldilocks::ONE);
-    const TWO: Self = Self::new(P3Goldilocks::TWO);
-    const NEG_ONE: Self = Self::new(P3Goldilocks::NEG_ONE);
-    const GENERATOR: Self = Self::new(P3Goldilocks::GENERATOR);
+    const ZERO: Self = Self::new(0);
+    const ONE: Self = Self::new(1);
+    const TWO: Self = Self::new(2);
+    const NEG_ONE: Self = Self::new(Self::ORDER_U64 - 1);
+    const GENERATOR: Self = Self::new(7);
 
     #[must_use]
     fn try_inverse(&self) -> Option<Self> {
         if self.is_zero() {
             return None;
         }
-        Some(Self::new(self.value.inverse()))
+        Some(unsafe { Self::new(goldilocks_inv_ffi(&self.0)) })
     }
 }
 
 impl PrimeField for Goldilocks {
     fn as_canonical_biguint(&self) -> BigUint {
-        P3PrimeField64::as_canonical_u64(&self.value).into()
+        self.0.into()
     }
 }
 
 impl PrimeField64 for Goldilocks {
-    const ORDER_U64: u64 = P;
+    const ORDER_U64: u64 = Self::P;
 
     #[inline]
     fn as_canonical_u64(&self) -> u64 {
-        P3Goldilocks::as_canonical_u64(&self.value)
+        let mut c = self.0;
+        if c >= Self::ORDER_U64 {
+            c -= Self::ORDER_U64;
+        }
+        c
     }
 }
 
 impl Add for Goldilocks {
     type Output = Self;
 
-    #[inline]
+    #[inline(always)]
     fn add(self, rhs: Self) -> Self {
-        Self::new(self.value + rhs.value)
+        #[cfg(target_arch = "x86_64")]
+        {
+            Self(unsafe { goldilocks_add_ffi(&self.0, &rhs.0) })
+        }
+
+        #[cfg(not(target_arch = "x86_64"))]
+        {
+            unreachable!("non-x86_64 architecture not supported");
+        }
     }
 }
 
 impl AddAssign for Goldilocks {
     #[inline]
     fn add_assign(&mut self, rhs: Self) {
-        self.value = self.value + rhs.value;
+        unsafe { goldilocks_add_assign_ffi(&mut self.0, &self.0, &rhs.0) }
     }
 }
 
@@ -170,14 +176,22 @@ impl Sub for Goldilocks {
 
     #[inline]
     fn sub(self, rhs: Self) -> Self {
-        Self::new(self.value - rhs.value)
+        #[cfg(target_arch = "x86_64")]
+        {
+            Self(unsafe { goldilocks_sub_ffi(&self.0, &rhs.0) })
+        }
+
+        #[cfg(not(target_arch = "x86_64"))]
+        {
+            unreachable!("non-x86_64 architecture not supported");
+        }
     }
 }
 
 impl SubAssign for Goldilocks {
     #[inline]
     fn sub_assign(&mut self, rhs: Self) {
-        self.value = self.value - rhs.value;
+        unsafe { goldilocks_sub_assign_ffi(&mut self.0, &self.0, &rhs.0) }
     }
 }
 
@@ -186,7 +200,7 @@ impl Neg for Goldilocks {
 
     #[inline]
     fn neg(self) -> Self::Output {
-        Self::new(P3Goldilocks::neg(self.value))
+        Self(unsafe { goldilocks_neg_ffi(&self.0) })
     }
 }
 
@@ -195,14 +209,22 @@ impl Mul for Goldilocks {
 
     #[inline]
     fn mul(self, rhs: Self) -> Self {
-        Self::new(self.value * rhs.value)
+        #[cfg(target_arch = "x86_64")]
+        {
+            Self(unsafe { goldilocks_mul_ffi(&self.0, &rhs.0) })
+        }
+
+        #[cfg(not(target_arch = "x86_64"))]
+        {
+            unreachable!("non-x86_64 architecture not supported");
+        }
     }
 }
 
 impl MulAssign for Goldilocks {
     #[inline]
     fn mul_assign(&mut self, rhs: Self) {
-        self.value = self.value * rhs.value;
+        unsafe { goldilocks_mul_assign_ffi(&mut self.0, &self.0, &rhs.0) }
     }
 }
 
@@ -211,14 +233,22 @@ impl Div for Goldilocks {
 
     #[allow(clippy::suspicious_arithmetic_impl)]
     fn div(self, rhs: Self) -> Self {
-        Self::new(P3Goldilocks::div(self.value, rhs.value))
+        #[cfg(target_arch = "x86_64")]
+        {
+            Self(unsafe { goldilocks_div_ffi(&self.0, &rhs.0) })
+        }
+
+        #[cfg(not(target_arch = "x86_64"))]
+        {
+            unreachable!("non-x86_64 architecture not supported");
+        }
     }
 }
 
 impl DivAssign for Goldilocks {
     #[allow(clippy::suspicious_arithmetic_impl)]
     fn div_assign(&mut self, rhs: Self) {
-        *self = *self / rhs;
+        unsafe { goldilocks_div_assign_ffi(&mut self.0, &self.0, &rhs.0) }
     }
 }
 
@@ -231,5 +261,188 @@ impl Distribution<Goldilocks> for StandardUniform {
                 return Goldilocks::from_u64(next_u64);
             }
         }
+    }
+}
+
+mod tests {
+    #[allow(unused_imports)]
+    use super::*;
+
+    #[test]
+    pub fn test_add_neg_sub_mul()
+    where
+        StandardUniform: Distribution<Goldilocks>,
+    {
+        let mut rng = rand::rng();
+        let x = rng.random::<Goldilocks>();
+        let y = rng.random::<Goldilocks>();
+        let z = rng.random::<Goldilocks>();
+        assert_eq!(Goldilocks::ONE + Goldilocks::NEG_ONE, Goldilocks::ZERO);
+        assert_eq!(x + (-x), Goldilocks::ZERO);
+        assert_eq!(Goldilocks::ONE + Goldilocks::ONE, Goldilocks::TWO);
+        assert_eq!(-x, Goldilocks::ZERO - x);
+        assert_eq!(x + x, x * Goldilocks::TWO);
+        assert_eq!(x * Goldilocks::TWO, x.double());
+        // assert_eq!(x, x.halve() * Goldilocks::TWO);
+        assert_eq!(x * (-x), -x.square());
+        assert_eq!(x + y, y + x);
+        assert_eq!(x * Goldilocks::ZERO, Goldilocks::ZERO);
+        assert_eq!(x * Goldilocks::ONE, x);
+        assert_eq!(x * y, y * x);
+        assert_eq!(x * (y * z), (x * y) * z);
+        assert_eq!(x - (y + z), (x - y) - z);
+        assert_eq!((x + y) - z, x + (y - z));
+        assert_eq!(x * (y + z), x * y + x * z);
+        // assert_eq!(x + y + z + x + y + z, [x, x, y, y, z, z].into_iter().sum());
+    }
+    #[test]
+    pub fn test_inv_div()
+    where
+        StandardUniform: Distribution<Goldilocks>,
+    {
+        let mut rng = rand::rng();
+        let x = rng.random::<Goldilocks>();
+        let y = rng.random::<Goldilocks>();
+        let z = rng.random::<Goldilocks>();
+        assert_eq!(x * x.inverse(), Goldilocks::ONE);
+        assert_eq!(x.inverse() * x, Goldilocks::ONE);
+        assert_eq!(x.square().inverse(), x.inverse().square());
+        assert_eq!((x / y) * y, x);
+        assert_eq!(x / (y * z), (x / y) / z);
+        assert_eq!((x * y) / z, x * (y / z));
+    }
+
+    #[test]
+    pub fn test_inverse()
+    where
+        StandardUniform: Distribution<Goldilocks>,
+    {
+        assert_eq!(None, Goldilocks::ZERO.try_inverse());
+
+        assert_eq!(Some(Goldilocks::ONE), Goldilocks::ONE.try_inverse());
+
+        let mut rng = rand::rng();
+        for _ in 0..1000 {
+            let x = rng.random::<Goldilocks>();
+            if !x.is_zero() && !x.is_one() {
+                let z = x.inverse();
+                assert_ne!(x, z);
+                assert_eq!(x * z, Goldilocks::ONE);
+            }
+        }
+    }
+
+    #[test]
+    pub fn test_from_int()
+    where
+        StandardUniform: Distribution<Goldilocks>,
+    {
+        assert_eq!(Goldilocks::from_int(0), Goldilocks::ZERO);
+        assert_eq!(Goldilocks::from_int(1), Goldilocks::ONE);
+        let field_two = Goldilocks::ONE + Goldilocks::ONE;
+        assert_eq!(Goldilocks::from_int(2), field_two);
+        assert_eq!(Goldilocks::from_int(2), Goldilocks::TWO);
+        let field_three = field_two + Goldilocks::ONE;
+        assert_eq!(Goldilocks::from_int(3), field_three);
+        assert_eq!(Goldilocks::from_int(3), Goldilocks::TWO + Goldilocks::ONE);
+        let field_six = field_two * field_three;
+        assert_eq!(Goldilocks::from_int(6), field_six);
+        let field_36 = field_six * field_six;
+        assert_eq!(Goldilocks::from_int(36), field_36);
+        let field_108 = field_36 * field_three;
+        assert_eq!(Goldilocks::from_int(108), field_108);
+
+        let field_neg_one = -Goldilocks::ONE;
+        assert_eq!(Goldilocks::from_int(-1), field_neg_one);
+        assert_eq!(Goldilocks::from_int(-1), Goldilocks::NEG_ONE);
+        let field_neg_two = field_neg_one + field_neg_one;
+        assert_eq!(Goldilocks::from_int(-2), field_neg_two);
+        let field_neg_four = field_neg_two + field_neg_two;
+        assert_eq!(Goldilocks::from_int(-4), field_neg_four);
+        let field_neg_six = field_neg_two + field_neg_four;
+        assert_eq!(Goldilocks::from_int(-6), field_neg_six);
+        let field_neg_24 = -field_neg_six * field_neg_four;
+        assert_eq!(Goldilocks::from_int(-24), field_neg_24);
+
+        let mut rng = rand::rng();
+        for _ in 0..1000 {
+            let x = rng.random::<Goldilocks>();
+            assert_eq!(x, Goldilocks::from_int(x.as_canonical_u64()));
+            assert_eq!(x, Goldilocks::from_canonical_checked(x.as_canonical_u64()).unwrap());
+            assert_eq!(x, unsafe { Goldilocks::from_canonical_unchecked(x.as_canonical_u64()) });
+        }
+    }
+
+    #[test]
+    fn test_as_canonical_u64() {
+        let mut rng = rand::rng();
+        let x: u64 = rng.random();
+        let x_mod_order = x % Goldilocks::ORDER_U64;
+
+        assert_eq!(Goldilocks::ZERO.as_canonical_u64(), 0);
+        assert_eq!(Goldilocks::ONE.as_canonical_u64(), 1);
+        assert_eq!(Goldilocks::TWO.as_canonical_u64(), 2 % Goldilocks::ORDER_U64);
+        assert_eq!(Goldilocks::NEG_ONE.as_canonical_u64(), Goldilocks::ORDER_U64 - 1);
+
+        assert_eq!(Goldilocks::from_int(Goldilocks::ORDER_U64).as_canonical_u64(), 0);
+        assert_eq!(Goldilocks::from_int(x).as_canonical_u64(), x_mod_order);
+        assert_eq!(unsafe { Goldilocks::from_canonical_unchecked(x_mod_order).as_canonical_u64() }, x_mod_order);
+    }
+
+    #[test]
+    fn test_as_unique_u64() {
+        assert_ne!(Goldilocks::ZERO.to_unique_u64(), Goldilocks::ONE.to_unique_u64());
+        assert_ne!(Goldilocks::ZERO.to_unique_u64(), Goldilocks::NEG_ONE.to_unique_u64());
+        assert_eq!(Goldilocks::from_int(Goldilocks::ORDER_U64).to_unique_u64(), Goldilocks::ZERO.to_unique_u64());
+    }
+
+    #[test]
+    fn generate_from_large_u_int_tests() {
+        // Check some wraparound cases:
+        // Note that for unsigned integers, from_canonical_checked returns
+        // None when the input is bigger or equal to the field order.
+        // Similarly, from_canonical_unchecked may also return invalid results in these cases.
+        let field_order = Goldilocks::ORDER_U64 as u64;
+
+        // On the other hand, everything should work fine for field_order - 1 and (field_order + 1)/2.
+        assert_eq!(field_order - 1, (-Goldilocks::ONE).0);
+
+        let half = (field_order + 1) >> 1;
+        let field_half = (Goldilocks::ONE + Goldilocks::ONE).inverse();
+        assert_eq!(half, field_half.0);
+
+        // We check that from_canonical_checked returns None for large enough values
+        // but from_int is still correct.
+        assert_eq!(Goldilocks::from_int(field_order), Goldilocks::ZERO);
+        assert_eq!(Goldilocks::from_canonical_checked(field_order), None);
+        assert_eq!(Goldilocks::from_int(field_order + 1), Goldilocks::ONE);
+        assert_eq!(Goldilocks::from_canonical_checked(field_order + 1), None);
+        assert_eq!(Goldilocks::from_canonical_checked(u64::MAX), None);
+    }
+
+    #[test]
+    fn generate_from_large_i_int_tests() {
+        // Check some wraparound cases:
+        // Note that for unsigned integers, from_canonical_checked returns
+        // None when |input| is bigger than (field order - 1)/2 and from_canonical_unchecked
+        // may also return invalid results in these cases.
+        let neg_half = (Goldilocks::ORDER_U64 >> 1) as i64;
+        let half_as_neg_rep = -neg_half;
+
+        let field_half = (Goldilocks::ONE + Goldilocks::ONE).inverse();
+        let field_neg_half = field_half - Goldilocks::ONE;
+
+        assert_eq!(half_as_neg_rep, field_half.0 as i64);
+        assert_eq!(neg_half, field_neg_half.0 as i64);
+
+        // We check that from_canonical_checked returns None for large enough values but
+        // from_int is still correct.
+        let half = neg_half + 1;
+        assert_eq!(Goldilocks::from_int(half), field_half);
+        assert_eq!(Goldilocks::from_canonical_checked(half), None);
+        assert_eq!(Goldilocks::from_int(-half), field_neg_half);
+        assert_eq!(Goldilocks::from_canonical_checked(-half), None);
+        assert_eq!(Goldilocks::from_canonical_checked(i64::MAX), None);
+        assert_eq!(Goldilocks::from_canonical_checked(i64::MIN), None);
     }
 }
--- provers/starks-lib-c/src/lib.rs
+++ provers/starks-lib-c/src/lib.rs
@@ -1,3 +1,5 @@
+mod ffi_goldilocks;
 mod ffi_starks;
 
+pub use ffi_goldilocks::*;
 pub use ffi_starks::*;
