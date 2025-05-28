38ce82bd3af5ccb1bc4fc4a2549af01a8bd997c8
RogerTaule <roger.taulee@gmail.com>
2025-05-28 15:24:18 +0000

    Cargo fmt

--- proofman/src/proofman.rs
+++ proofman/src/proofman.rs
@@ -150,7 +150,6 @@ where
         input_data_path: Option<PathBuf>,
         debug_info: &DebugInfo,
     ) -> Result<(), Box<dyn std::error::Error>> {
-       
         timer_start_info!(EXECUTE);
         self.pctx.set_debug_info(debug_info.clone());
         self.wcm.set_input_data_path(input_data_path);
