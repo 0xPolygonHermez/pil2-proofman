13f118c2e7ace3940445df6e7dadc48ca6bc4c9b
rickb80 <75077385+rickb80@users.noreply.github.com>
2026-01-07 11:22:54 +0000

    test poseison2 bn128 passing

--- pil2-stark/src/bn128/src/poseidon2/poseidon2_bn128.cpp
+++ pil2-stark/src/bn128/src/poseidon2/poseidon2_bn128.cpp
@@ -9,31 +9,33 @@ void Poseidon2BN128::hash(vector<FrElement> &state, FrElement *result)
 void Poseidon2BN128::hash(vector<FrElement> &state)
 {
 
-	assert(state.size() < 18);
 	const int t = state.size();
-	assert(t == 4); // Currently only t=4 is supported
-	const int nRoundsP = N_ROUNDS_P[t - 1];
+	assert(t < 16);
+	const int nRoundsP = N_ROUNDS_P[t - 2];
 
-	const vector<FrElement> *c = &(Poseidon2BN128Constants::C[t - 1]);
-	const vector<FrElement> *d = &(Poseidon2BN128Constants::D[t - 1]);
+	const vector<FrElement> *c = &(Poseidon2BN128Constants::C[t - 2]);
+	const vector<FrElement> *d = &(Poseidon2BN128Constants::D[t - 2]);
 
 	matmul_external(&state[0], t);
+
+	
 	for (int r = 0; r < N_ROUNDS_F / 2; r++)
 	{
-		pow7add(&state[0], &((*c)[r * t]), t);
-		matmul_external(&state[0], t);
+		
+		pow5add(&state[0], &((*c)[r * t]), t);
+		matmul_external(&state[0], t);		
 	}
 	for (int r = 0; r < nRoundsP; r++)
 	{
 		field.add(state[0], state[0], (*c)[(N_ROUNDS_F / 2) * t + r]);
-		pow7(state[0]);
+		pow5(state[0]);
 		FrElement sum = field.zero();
 		add(sum, &state[0], t);
-		prodadd(&state[0], &((*d)[0]), sum, t);
+		prodadd(&state[0], &((*d)[0]), sum, t);		
 	}
 	for (int r = 0; r < N_ROUNDS_F / 2; r++)
 	{
-		pow7add(&state[0], &((*c)[(N_ROUNDS_F / 2) * t + nRoundsP + r * t]), t);
+		pow5add(&state[0], &((*c)[(N_ROUNDS_F / 2) * t + nRoundsP + r * t]), t);
 		matmul_external(&state[0], t);
 	}
 }
