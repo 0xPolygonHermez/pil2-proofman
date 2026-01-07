d5fd89f4863d667e9bcddba83dfc95ebd0964144
rickb80 <75077385+rickb80@users.noreply.github.com>
2026-01-07 17:32:58 +0000

    bn128 poseidon2 gpu implemented

--- pil2-stark/src/bn128/src/ffigpu/fr.cuh
+++ pil2-stark/src/bn128/src/ffigpu/fr.cuh
@@ -1,3 +1,6 @@
+#ifndef __BN128_FFIGPU_FR_CUH__
+#define __BN128_FFIGPU_FR_CUH__
+
 #define FEATURE_BN254
 # if defined(__CUDA_ARCH__) || defined(__HIPCC__)
 #  include <alt_bn128.hpp>
@@ -10,10 +13,12 @@ public:
     struct Element {
         alt_bn128::fr_t v;
     };
-
+    static __device__ __forceinline__ Element zero();
+    static __device__ __forceinline__ void copy(Element& r, const Element& a);
     static __device__ __forceinline__ void add(Element& r, const Element& a, const Element& b);
     static __device__ __forceinline__ void sub(Element& r, const Element& a, const Element& b);
     static __device__ __forceinline__ void mul(Element& r, const Element& a, const Element& b);
+    static __device__ __forceinline__ void square(Element& r, const Element& a);
 
     static __device__ __forceinline__ Element add(const Element& a, const Element& b);
     static __device__ __forceinline__ Element sub(const Element& a, const Element& b);
@@ -21,6 +26,17 @@ public:
 
 };
 #if defined(__CUDACC__) && defined(__CUDA_ARCH__)
+__device__ __forceinline__ BN128GPUScalarField::Element BN128GPUScalarField::zero() {
+    Element r;
+    r.v[0] = 0;
+    r.v[1] = 0;
+    r.v[2] = 0;
+    r.v[3] = 0;
+    return r;
+}
+__device__ __forceinline__ void BN128GPUScalarField::copy(Element& r, const Element& a) {
+    r.v = a.v;
+}
 __device__ __forceinline__ void BN128GPUScalarField::add(Element& r, const Element& a, const Element& b) {
     r.v = a.v;
     r.v += b.v;
@@ -44,4 +60,8 @@ __device__ __forceinline__ BN128GPUScalarField::Element BN128GPUScalarField::sub
 __device__ __forceinline__ BN128GPUScalarField::Element BN128GPUScalarField::mul(const Element& a, const Element& b) {    
     return {a.v * b.v};
 }
+__device__ __forceinline__ void BN128GPUScalarField::square(Element& r, const Element& a) {
+    r.v = sqr(a.v);
+}
 #endif
+#endif // __BN128_FFIGPU_FR_CUH__
