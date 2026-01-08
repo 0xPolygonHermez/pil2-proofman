ec227a3d5c90f5de187e8137866dac5d765a2e83
rickb80 <75077385+rickb80@users.noreply.github.com>
2026-01-08 14:13:08 +0000

    tests for poseidon2 bn128 gpu passing

--- pil2-stark/src/bn128/src/poseidon2/poseidon2_bn128.cu
+++ pil2-stark/src/bn128/src/poseidon2/poseidon2_bn128.cu
@@ -1,52 +1,118 @@
+#include "cuda_utils.cuh"
 #include "poseidon2_bn128.cuh"
-
-// Device constant memory definitions
-__device__ __constant__ int N_ROUNDS_F = 8;
-__device__ __constant__ int N_ROUNDS_P[6] = {56, 56, 56, 57, 57, 57};
-
-void Poseidon2BN128GPU::hash(vector<FrElement> &state, FrElement *result)
-{
-#if 0
-	hash(state);
-	*result = state[0];
-#endif
-}
-
-void Poseidon2BN128GPU::hash(vector<FrElement> &state)
-{
-	//hash<1,1>(&state[0], nullptr, nullptr, state.size()); //do it better
-}
+#include "poseidon2_bn128_constants.cuh"  // Host-side constants
+#include <cuda_runtime.h>
 
 typedef Poseidon2BN128GPU::FrElement FrElementGPU;
 
-__global__ void poseidon2_hash_kernel(FrElementGPU *state, FrElementGPU* C, FrElementGPU* D, int t){
+// Device constant memory - uninitialized, will be copied at runtime
+__device__ __constant__ FrElementGPU GPU_C2[72];
+__device__ __constant__ FrElementGPU GPU_D2[2];
+__device__ __constant__ FrElementGPU GPU_C3[80];
+__device__ __constant__ FrElementGPU GPU_D3[3];
+__device__ __constant__ FrElementGPU GPU_C4[88];
+__device__ __constant__ FrElementGPU GPU_D4[4];
+__device__ __constant__ FrElementGPU GPU_C8[121];
+__device__ __constant__ FrElementGPU GPU_D8[8];
+__device__ __constant__ FrElementGPU GPU_C12[153];
+__device__ __constant__ FrElementGPU GPU_D12[12];
+__device__ __constant__ FrElementGPU GPU_C16[185];
+__device__ __constant__ FrElementGPU GPU_D16[16];
 
-	Poseidon2BN128GPU poseidon;
+// Round counts
+__device__ __constant__ int N_ROUNDS_F = 8;
+__device__ __constant__ int N_ROUNDS_P[6] = {56, 56, 56, 57, 57, 57};
 
-	//uint32_t pos = t<=4 ? t-2 : t/4 + 1;
-	const int nRoundsP = N_ROUNDS_P[0];
-	poseidon.matmul_external(&state[0], t);
+// Helper to get C constants pointer based on t
+__device__ __forceinline__ const FrElementGPU* get_C(int t) {
+    switch(t) {
+        case 2:  return GPU_C2;
+        case 3:  return GPU_C3;
+        case 4:  return GPU_C4;
+        case 8:  return GPU_C8;
+        case 12: return GPU_C12;
+        case 16: return GPU_C16;
+        default: return nullptr;
+    }
+}
 
-	for (int r = 0; r < N_ROUNDS_F / 2; r++)
-	{		
-		poseidon.pow5add(&state[0], &C[r * t], t);
-		poseidon.matmul_external(&state[0], t);		
-	}
-	for (int r = 0; r < nRoundsP; r++)
+// Helper to get D constants pointer based on t
+__device__ __forceinline__ const FrElementGPU* get_D(int t) {
+    switch(t) {
+        case 2:  return GPU_D2;
+        case 3:  return GPU_D3;
+        case 4:  return GPU_D4;
+        case 8:  return GPU_D8;
+        case 12: return GPU_D12;
+        case 16: return GPU_D16;
+        default: return nullptr;
+    }
+}
+
+
+__global__ void poseidon2_hash_kernel(FrElementGPU *state, int t) {
+    Poseidon2BN128GPU poseidon;
+    const FrElementGPU *C = get_C(t);
+    const FrElementGPU *D = get_D(t);
+	uint32_t pos = t<=4 ? t-2 : t/4 + 1;
+    const int nRoundsP = N_ROUNDS_P[pos];
+
+    poseidon.matmul_external(&state[0], t);
+
+    for (int r = 0; r < N_ROUNDS_F / 2; r++) {
+        poseidon.pow5add(&state[0], &C[r * t], t);
+        poseidon.matmul_external(&state[0], t);
+    }
+    for (int r = 0; r < nRoundsP; r++) {
+        BN128GPUScalarField::add(state[0], state[0], C[(N_ROUNDS_F / 2) * t + r]);
+        poseidon.pow5(state[0]);
+        FrElementGPU sum = BN128GPUScalarField::zero();
+        poseidon.add(sum, &state[0], t);
+        poseidon.prodadd(&state[0], &D[0], sum, t);
+    }
+    for (int r = 0; r < N_ROUNDS_F / 2; r++) {
+        poseidon.pow5add(&state[0], &C[(N_ROUNDS_F / 2) * t + nRoundsP + r * t], t);
+        poseidon.matmul_external(&state[0], t);
+    }
+}
+
+void Poseidon2BN128GPU::hash(FrElement* d_state, int t) {
+    poseidon2_hash_kernel<<<1, 1>>>(d_state, t);
+}
+
+// Initialize GPU constants by copying from host
+void Poseidon2BN128GPU::initGPUConstants(uint32_t* gpu_ids, uint32_t num_gpu_ids) {
+    static bool initialized = false;
+    if (initialized) return;
+    
+    initialized = true;
+	int deviceId;
+    CHECKCUDAERR(cudaGetDevice(&deviceId));
+
+	for(int i = 0; i < num_gpu_ids; i++)
 	{
-		BN128GPUScalarField::add(state[0], state[0], C[(N_ROUNDS_F / 2) * t + r]);
-		poseidon.pow5(state[0]);
-		FrElementGPU sum = BN128GPUScalarField::zero();
-		poseidon.add(sum, &state[0], t);
-		poseidon.prodadd(&state[0], &D[0], sum, t);		
-	}
-	for (int r = 0; r < N_ROUNDS_F / 2; r++)
-	{
-		poseidon.pow5add(&state[0], &C[(N_ROUNDS_F / 2) * t + nRoundsP + r * t], t);
-		poseidon.matmul_external(&state[0], t);
-	}
- }
+		CHECKCUDAERR(cudaSetDevice(gpu_ids[i]));
 
+		// Copy C constants
+		CHECKCUDAERR(cudaMemcpyToSymbol(GPU_C2, Poseidon2BN128ConstantsGPU::C2, sizeof(Poseidon2BN128ConstantsGPU::C2), 0, cudaMemcpyHostToDevice));
+		CHECKCUDAERR(cudaMemcpyToSymbol(GPU_C3, Poseidon2BN128ConstantsGPU::C3, sizeof(Poseidon2BN128ConstantsGPU::C3), 0, cudaMemcpyHostToDevice));
+		CHECKCUDAERR(cudaMemcpyToSymbol(GPU_C4, Poseidon2BN128ConstantsGPU::C4, sizeof(Poseidon2BN128ConstantsGPU::C4), 0, cudaMemcpyHostToDevice));
+		CHECKCUDAERR(cudaMemcpyToSymbol(GPU_C8, Poseidon2BN128ConstantsGPU::C8, sizeof(Poseidon2BN128ConstantsGPU::C8), 0, cudaMemcpyHostToDevice));
+		CHECKCUDAERR(cudaMemcpyToSymbol(GPU_C12, Poseidon2BN128ConstantsGPU::C12, sizeof(Poseidon2BN128ConstantsGPU::C12), 0, cudaMemcpyHostToDevice));
+		CHECKCUDAERR(cudaMemcpyToSymbol(GPU_C16, Poseidon2BN128ConstantsGPU::C16, sizeof(Poseidon2BN128ConstantsGPU::C16), 0, cudaMemcpyHostToDevice));
+
+		// Copy D constants
+		CHECKCUDAERR(cudaMemcpyToSymbol(GPU_D2, Poseidon2BN128ConstantsGPU::D2, sizeof(Poseidon2BN128ConstantsGPU::D2), 0, cudaMemcpyHostToDevice));
+		CHECKCUDAERR(cudaMemcpyToSymbol(GPU_D3, Poseidon2BN128ConstantsGPU::D3, sizeof(Poseidon2BN128ConstantsGPU::D3), 0, cudaMemcpyHostToDevice));
+		CHECKCUDAERR(cudaMemcpyToSymbol(GPU_D4, Poseidon2BN128ConstantsGPU::D4, sizeof(Poseidon2BN128ConstantsGPU::D4), 0, cudaMemcpyHostToDevice));
+		CHECKCUDAERR(cudaMemcpyToSymbol(GPU_D8, Poseidon2BN128ConstantsGPU::D8, sizeof(Poseidon2BN128ConstantsGPU::D8), 0, cudaMemcpyHostToDevice));
+		CHECKCUDAERR(cudaMemcpyToSymbol(GPU_D12, Poseidon2BN128ConstantsGPU::D12, sizeof(Poseidon2BN128ConstantsGPU::D12), 0, cudaMemcpyHostToDevice));
+		CHECKCUDAERR(cudaMemcpyToSymbol(GPU_D16, Poseidon2BN128ConstantsGPU::D16, sizeof(Poseidon2BN128ConstantsGPU::D16), 0, cudaMemcpyHostToDevice));
+
+	}       
+    
+    cudaSetDevice(deviceId);
+}
 
 
 
