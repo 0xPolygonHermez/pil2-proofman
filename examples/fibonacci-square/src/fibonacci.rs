c5d5b26abbdb2b3ee15583aa198aee4dd4d786d2
Roger Taulé Buxadera <roger.taulee@gmail.com>
2026-02-05 10:57:58 +0100

    Rom setup gpu sdk (#415)

--- examples/fibonacci-square/src/fibonacci.rs
+++ examples/fibonacci-square/src/fibonacci.rs
@@ -85,7 +85,7 @@ impl<F: PrimeField64> WitnessComponent<F> for FibonacciSquare {
 
         let setup = sctx.get_setup(trace_rom.airgroup_id(), trace_rom.air_id())?;
         let blowup_factor = 1 << (setup.stark_info.stark_struct.n_bits_ext - setup.stark_info.stark_struct.n_bits);
-        write_custom_commit_trace::<F>(&mut trace_rom, blowup_factor, MERKLE_TREE_ARITY, &file_name)?;
+        write_custom_commit_trace::<F>(&pctx, &mut trace_rom, blowup_factor, MERKLE_TREE_ARITY, &file_name)?;
         Ok(())
     }
 }
