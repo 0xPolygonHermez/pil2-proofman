f79b2776f6cf5e87d5c1fac73655febd24b33037
RogerTaule <roger.taulee@gmail.com>
2026-02-16 10:43:59 +0000

    Cargo clippy

--- pil2-components/lib/std/rs/src/common.rs
+++ pil2-components/lib/std/rs/src/common.rs
@@ -130,7 +130,7 @@ pub fn parse_debug_values_to_hashes<F: PrimeField64>(pctx: &Arc<ProofCtx<F>>) ->
         }
 
         let norm_vals = normalize_vals(&parsed_values);
-        let hash = hash_vals(&norm_vals);
+        let hash = hash_vals(norm_vals);
         hashes.push(hash);
     }
 
