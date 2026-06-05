6cacd753ceaa180576497061ab8ba72f97e2a3f1
rickb80 <75077385+rickb80@users.noreply.github.com>
2026-06-05 08:09:40 +0000

    poseidon1: set default arity to 4

--- provers/starks-lib-c/src/lib.rs
+++ provers/starks-lib-c/src/lib.rs
@@ -5,7 +5,7 @@ pub use ffi_starks::*;
 
 #[doc(hidden)]
 #[cfg(feature = "stark-poseidon1")]
-pub const GOLDILOCKS_MERKLE_TREE_ARITY: u64 = 3;
+pub const GOLDILOCKS_MERKLE_TREE_ARITY: u64 = 4;
 #[doc(hidden)]
 #[cfg(not(feature = "stark-poseidon1"))]
 pub const GOLDILOCKS_MERKLE_TREE_ARITY: u64 = 4;
