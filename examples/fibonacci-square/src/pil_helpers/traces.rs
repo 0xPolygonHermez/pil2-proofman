8207e54a8588563db4da8952189af2118d568c62
RogerTaule <roger.taulee@gmail.com>
2026-06-23 15:24:08 +0000

    Reducing cells recursion

--- examples/fibonacci-square/src/pil_helpers/traces.rs
+++ examples/fibonacci-square/src/pil_helpers/traces.rs
@@ -16,7 +16,7 @@ use std::fmt;
 #[allow(dead_code)]
 type FieldExtension<F> = [F; 3];
 
-pub const PILOUT_HASH: &str = "c9361fdf6122c9530e1dab1436fbeb1ed8f3475a3dc2d3f32cf44479a1e425eb";
+pub const PILOUT_HASH: &str = "61c30ca239725b8110ed74680b763a535e5d3f340897c1f4d4c45720bc17bc12";
 
 pub const MERKLE_TREE_ARITY: u64 = 4;
 
