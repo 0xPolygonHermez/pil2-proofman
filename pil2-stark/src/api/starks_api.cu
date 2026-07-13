39a299cf12983ec53ca615327501e91424b4c4d2
Roger Taulé i Buxadera <roger.taulee@gmail.com>
2026-07-13 08:59:56 +0200

    Fix

--- pil2-stark/src/api/starks_api.cu
+++ pil2-stark/src/api/starks_api.cu
@@ -1803,13 +1803,11 @@ uint32_t selectStream(DeviceCommitBuffers* d_buffers, uint64_t airgroupId, uint6
     uint32_t countFreeStreamsGPU[d_buffers->n_gpus];
     uint32_t countUnusedStreams[d_buffers->n_gpus];
     int streamIdxGPU[d_buffers->n_gpus];
-    bool foundReusableContext[d_buffers->n_gpus];
 
     for( uint32_t i = 0; i < d_buffers->n_gpus; i++){
         countUnusedStreams[i] = 0;
         countFreeStreamsGPU[i] = 0;
         streamIdxGPU[i] = -1;
-        foundReusableContext[i] = false;
     }
 
     bool someFree = false;
@@ -1833,19 +1831,14 @@ uint32_t selectStream(DeviceCommitBuffers* d_buffers, uint64_t airgroupId, uint6
                     }
                     if (d_buffers->streamsData[i].status==0 || d_buffers->streamsData[i].status==3 || (d_buffers->streamsData[i].status==2 &&  cudaEventQuery(d_buffers->streamsData[i].end_event) == cudaSuccess)) {
                         uint32_t gpuLocalId = d_buffers->gpus_g2l[d_buffers->streamsData[i].gpuId];
-                        bool sameContext = d_buffers->streamsData[i].airgroupId == airgroupId && d_buffers->streamsData[i].airId == airId && d_buffers->streamsData[i].proofType == proofType;
 
                         countFreeStreamsGPU[gpuLocalId]++;
-                        // Prefer a stream already holding this (airgroup, air, proofType) context
-                        // even if it is reusable-but-not-unused (status 2/3): keeps constant
-                        // buffers resident and avoids re-uploading them.
-                        if (sameContext) {
-                            streamIdxGPU[gpuLocalId] = i;
-                            foundReusableContext[gpuLocalId] = true;
-                        }
                         if(d_buffers->streamsData[i].status==0){
                             countUnusedStreams[gpuLocalId]++;
-                            if (!foundReusableContext[gpuLocalId]) streamIdxGPU[gpuLocalId] = i;
+                            streamIdxGPU[gpuLocalId] = i;
+                        }
+                        if (d_buffers->streamsData[i].airgroupId == airgroupId && d_buffers->streamsData[i].airId == airId && d_buffers->streamsData[i].proofType == proofType && (d_buffers->streamsData[i].status==0 || d_buffers->streamsData[i].status==3)){
+                            streamIdxGPU[gpuLocalId] = i;
                         }
                         if( streamIdxGPU[gpuLocalId] == -1 ){
                             streamIdxGPU[gpuLocalId] = i;
@@ -1871,18 +1864,14 @@ uint32_t selectStream(DeviceCommitBuffers* d_buffers, uint64_t airgroupId, uint6
                     }
                     if (d_buffers->streamsData[i].status==0 || d_buffers->streamsData[i].status==3 || (d_buffers->streamsData[i].status==2 &&  cudaEventQuery(d_buffers->streamsData[i].end_event) == cudaSuccess)) {
                         uint32_t gpuLocalId = d_buffers->gpus_g2l[d_buffers->streamsData[i].gpuId];
-                        bool sameContext = d_buffers->streamsData[i].airgroupId == airgroupId && d_buffers->streamsData[i].airId == airId && d_buffers->streamsData[i].proofType == proofType;
 
                         countFreeStreamsGPU[gpuLocalId]++;
-                        // Prefer a stream already holding this (airgroup, air, proofType) context
-                        // even if it is reusable-but-not-unused (status 2/3).
-                        if (sameContext) {
-                            streamIdxGPU[gpuLocalId] = i;
-                            foundReusableContext[gpuLocalId] = true;
-                        }
                         if(d_buffers->streamsData[i].status==0){
                             countUnusedStreams[gpuLocalId]++;
-                            if (!foundReusableContext[gpuLocalId]) streamIdxGPU[gpuLocalId] = i;
+                            streamIdxGPU[gpuLocalId] = i;
+                        }
+                        if (d_buffers->streamsData[i].airgroupId == airgroupId && d_buffers->streamsData[i].airId == airId && d_buffers->streamsData[i].proofType == proofType && (d_buffers->streamsData[i].status==0 || d_buffers->streamsData[i].status==3)){
+                            streamIdxGPU[gpuLocalId] = i;
                         }
                         if( streamIdxGPU[gpuLocalId] == -1 ){
                             streamIdxGPU[gpuLocalId] = i;
