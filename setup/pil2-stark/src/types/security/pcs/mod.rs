4188688f31283b98ec498f2b4cb9bcf69baafcf6
Héctor Masip Ardevol <68732820+hecmas@users.noreply.github.com>
2026-08-26 16:11:55 +0000

    Adding soundness parameters of STIR

--- setup/pil2-stark/src/types/security/pcs/mod.rs
+++ setup/pil2-stark/src/types/security/pcs/mod.rs
@@ -1,7 +1,9 @@
 mod fri;
-mod whir;
+mod stir;
 mod types;
+mod whir;
 
 pub use fri::{Fri, FriConfig, FriSecurityParams};
+pub use stir::{Stir, StirConfig, StirSecurityParams};
 pub use types::{Batching, Pcs};
 pub use whir::{Whir, WhirConfig, WhirSecurityParams, whir_security_per_query};
